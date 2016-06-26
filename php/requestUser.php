d6a67e2a6459332ffcf661913747c349f44ed616
maxy2020 <maximilianodanelutti@gmail.com>
2016-06-26 16:00:54 -0300

    Request User AJAX (back)

---
+++ php/requestUser.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "DBConnection.php";
+require_once 'classes/User.php';
+
+header('Content-Type: application/json; charset=utf-8');
+
+
+if($_SERVER['REQUEST_METHOD'] == "GET") {
+	if(isset($_GET["id"])){
+		echo json_encode(User::getById($_GET["id"]));
+	}
+	else{
+		echo json_encode(User::getAll());
+	}
+}
+elseif($_SERVER['REQUEST_METHOD'] == "POST") {
+	if($_POST['email'] && $_POST['email'] && $_POST['email']){
+		try {
+			$user = new User($_POST['email'],$_POST['email'],$_POST['email']);
+			echo $user->addNew();
+		} catch (Exception $e) {
+    		echo json_encode({'error'=>$e->getMessage()});
+		}
+	}
+	else {
+		return json_encode({"error"=>"error"});
+	}
+}
+else {	
+	header('Location: http://www.example.com/');
+	exit;
+}
