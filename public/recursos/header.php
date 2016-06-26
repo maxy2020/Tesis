44d92fed37c29ad9a2af00b374dce1fc485395ca
qwegon <qwegon@hotmail.com>
2016-06-26 15:58:06 -0300

    creacion de directorios, header y footer

---
+++ public/recursos/header.php
@@ -0,0 +1,31 @@
+<header>
+		<div class="center">
+			<h1><a href="index.html">Indie Games</a></h1>
+			<div class="menu">
+				<div class="btn">Categorías</div>
+				<div class="categories">
+					<ul>
+						<li class="car"><a href="listado.html">Autos</a></li>
+						<li class="car"><a href="listado.html">Autos</a></li>
+						<li class="car"><a href="listado.html">Autos</a></li>
+						<li class="car"><a href="listado.html">Autos</a></li>
+						<li class="car"><a href="listado.html">Autos</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="right">
+				<div class="search"><input type="search" /><button>BUSCAR</button></div>
+				<div class="login">
+					<div class="form">
+						<input type="text" />
+						<input type="password" />
+						<input type="submit" value="Entrar" />
+					</div>
+					<div class="extras">
+						<a href="registro.html">Registrarme</a>
+						<a href="#">Olvide mi Pass</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
